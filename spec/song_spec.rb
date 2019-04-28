926c36124a44405ec68a7c203f29de4f70fadbe2
Lisa Scott <lisarenesoctt@gmail.com>
2019-04-28 16:26:03 -0400

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,22 +1,22 @@
 require "spec_helper"
 
 describe "Song" do
-  let!(:song) { Song.new } 
+  let!(:song) { Song.new }
   it "has a title" do
-    song.title = "7/11" 
+    song.title = "7/11"
     expect(song.title).to eq("7/11")
   end
 
-  it "belongs to an artist" do 
+  it "belongs to an artist" do
     artist = Artist.new
     song.artist = artist
     expect(song.artist).to eq(artist)
   end
 
-  it "knows the name of the artist it belongs to" do 
-    artist = Artist.new
-    artist.name = "Beyonce"
-    song.artist = artist
+  it "knows the name of the artist it belongs to" do
+    a1 = Artist.new
+    a1.name = "Beyonce"
+    song.artist = a1
     expect(song.artist.name).to eq("Beyonce")
   end
 end
